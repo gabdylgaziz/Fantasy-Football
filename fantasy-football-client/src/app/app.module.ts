24b1356edd9bd1d6ebde7ae9b194ae89f8bed16f
Aldiar Saparov <saparovaldiar1@gmail.com>
2023-04-21 21:24:17 +0600

    added first page

--- fantasy-football-client/src/app/app.module.ts
+++ fantasy-football-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ToptableComponent } from './toptable/toptable.component';
+import { NewsComponent } from './news/news.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ToptableComponent,
+    NewsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
