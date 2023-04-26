699205692d6f1dd212e08eb91e366308a937a08f
gabdylgaziz <g_zhagypar@kbtu.kz>
2023-04-26 14:43:36 +0600

    added routes and pages

--- fantasy-football-client/src/app/app-routing.module.ts
+++ fantasy-football-client/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainpageComponent } from './mainpage/mainpage.component'
 import { FantasyComponent } from './fantasy/fantasy.component';
+import { AccountComponent } from './account/account.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 
 const routes: Routes = [
+  {path: '', component: MainpageComponent},
+  {path: 'fantasy', component: FantasyComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'signup', component: SignupFormComponent}
 ];
 
 @NgModule({
