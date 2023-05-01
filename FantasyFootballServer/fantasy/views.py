6ede4560db1ad4fc77fcfa678fabbf1ea6ff7ac1
gabdylgaziz <g_zhagypar@kbtu.kz>
2023-05-01 19:37:58 +0600

    added first view

--- FantasyFootballServer/fantasy/views.py
+++ FantasyFootballServer/fantasy/views.py
@@ -1,5 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 
-
+def main(request):
+    return JsonResponse({'FIRST PAGE': 'HELLO WORLD'})
