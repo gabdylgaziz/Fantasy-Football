389a3d2b822359cda7e4813c7c775334c9a88421
gabdylgaziz <g_zhagypar@kbtu.kz>
2023-05-05 22:47:26 +0600

    added delete profile func

--- FantasyFootballServer/account/urls.py
+++ FantasyFootballServer/account/urls.py
@@ -8,12 +8,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     #path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     #path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register', RegisterView.as_view(), name="sign_up"),
-    path('login', LoginView.as_view(), name="login"),
-    path('user', UserView.as_view(), name="user_profile"),
-    path('logout', LogoutView.as_view(), name="logout"),
-    path('api/account/', AccountList.as_view()),
-    path('api/account/<int:id>/', AccountDetail.as_view()),
     path('register/', RegisterView.as_view(), name="sign_up"),
     path('login/', LoginView.as_view(), name="login"),
     path('user/<str:s>/', getProfile),
