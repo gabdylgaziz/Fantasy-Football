00a09fc9c76075b123e8be89baac9bda2fed4b7b
gabdylgaziz <g_zhagypar@kbtu.kz>
2023-05-05 20:21:59 +0600

    fantasy front updated

--- fantasy-football-client/src/app/fantasy/fantasy.component.ts
+++ fantasy-football-client/src/app/fantasy/fantasy.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../players';
+import { GK, MF, DF, FW } from '../players';
 
 @Component({
   selector: 'app-fantasy',
   templateUrl: './fantasy.component.html',
   styleUrls: ['./fantasy.component.css']
 })
-export class FantasyComponent {
-  
+export class FantasyComponent implements OnInit {
+  GK = GK
+  MF = MF 
+  DF = DF 
+  FW = FW
+  type: string;
+  constructor(){
+    this.type = 'None';
+  }
+  ngOnInit(){
+    
+  }
+
+  change(t: string){
+    this.type = t;
+  }
+
+  reset(){
+    this.type = 'None';
+  }
 }
---
+++ fantasy-football-client/src/app/players.ts
@@ -0,0 +1,95 @@
+export interface Player{
+    name: string;
+    cost: number;
+    rating: number;
+    src: string;
+}
+
+export const GK = [
+    {
+        name: "Ramsdale",
+        role: "GK",
+        cost: 4.9,
+        rating: 119,
+        src: "../../assets/images/fantasy/yellow_goalk_shirt.webp"
+    },
+    {
+        name: "Pope",
+        role: "GK",
+        cost: 5.4,
+        rating: 134,
+        src: "../../assets/images/fantasy/blue_goalk_shirt.webp"
+    },
+    {
+        name: "Alisson",
+        role: "GK",
+        cost: 5.4,
+        rating: 132,
+        src: "../../assets/images/fantasy/purple_goalk_shirt.webp"
+    },
+]
+
+export const DF = [
+    {
+        name: "Trippier",
+        role: "DF",
+        cost: 6.2,
+        rating: 173,
+        src: "../../assets/images/fantasy/striped_shirt.webp"
+    },
+    {
+        name: "White",
+        role: "DF",
+        cost: 5.0,
+        rating: 139,
+        src: "../../assets/images/fantasy/red_white_shirt.webp"
+    },
+]
+
+export const MF = [
+    {
+        name: "Salah",
+        role: "MF",
+        cost: 13.0,
+        rating: 187,
+        src: "../../assets/images/fantasy/red_shirt.webp"
+    },
+    {
+        name: "De Bruyne",
+        role: "MF",
+        cost: 12.1,
+        rating: 159,
+        src: "../../assets/images/fantasy/blue_shirt.webp"
+    },
+    {
+        name: "Eze",
+        role: "MF",
+        cost: 5.6,
+        rating: 125,
+        src: "../../assets/images/fantasy/blue_shirt.webp"
+    },
+]
+
+export const FW = [
+    {
+        name: "Haaland",
+        role: "FW",
+        cost: 12.3,
+        rating: 227,
+        src: "../../assets/images/fantasy/blue_shirt.webp"
+    },
+    {
+        name: "Kane",
+        role: "FW",
+        cost: 11.5,
+        rating: 206,
+        src: "../../assets/images/fantasy/white_shirt.webp"
+    },
+    {
+        name: "Toney",
+        role: "FW",
+        cost: 7.6,
+        rating: 171,
+        src: "../../assets/images/fantasy/striped_red_shirt.webp"
+    },
+]
